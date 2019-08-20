b2ab4387fa34c887676fb04c3de44e3d9133918a
Georg Braun <Georg.Braun92@gmail.com>
2019-08-20 17:00:49 +0200

    Refacotring, JsonEditor ergänzt

--- family-tree/src/App.js
+++ family-tree/src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./App.css";
 import FamilyGraph from "./FamilyGraph.js";
+import JSONInput from 'react-json-editor-ajrm';
+    import locale    from 'react-json-editor-ajrm/locale/en';
 
 class JsonEditor extends React.Component {
   render() {
@@ -20,22 +22,31 @@ class App extends React.Component {
       familyRelations: require("./familyRelations.json")
     };
 
-    this.JsonValueChanged = this.JsonValueChanged.bind(this);
+    this.handleJsonValueChange = this.handleJsonValueChange.bind(this);
   }
 
-  JsonValueChanged(event) {
-    var hParsedJson = JSON.parse(event.target.value);
-    this.setState({ familyRelations: hParsedJson });
+  handleJsonValueChange(event) {
+    this.setState({ familyRelations: event.jsObject });
   }
+
 
   render() {
     return (
       <div className="App">
         <h1>Stammbaum</h1>
-        <JsonEditor
-          value={this.state.familyRelations}
-          onChange={this.JsonValueChanged}
+        <div style={{ maxWidth: "1400px", maxHeight: "100%" }}>
+        <JSONInput
+          placeholder={this.state.familyRelations} // data to display
+          theme="dark_vscode_tribute"
+          locale = {locale}
+          colors={{
+            string: "#DAA520" // overrides theme colors with whatever color value you want
+          }}
+          height="550px"
+          onChange={this.handleJsonValueChange}
         />
+      </div>
+
         <FamilyGraph data={this.state.familyRelations} />
       </div>
     );
--- family-tree/src/FamilyGraph.js
+++ family-tree/src/FamilyGraph.js
@@ -1,5 +1,7 @@
 import React from "react";
 import dTree from "d3-dtree";
+import "./FamilyGraph.css"
+import 'jsoneditor-react/es/editor.min.css';
 
 class FamilyGraph extends React.Component {
   componentDidMount() {
@@ -52,7 +54,7 @@ class FamilyGraph extends React.Component {
 
     // Container vor erneutem Graph erstellen leeren.
     document.getElementById("graph").innerHTML = "";
-    dTree.init(data, this.graphOptions);
+    dTree.init(data, options);
   }
 }
 
