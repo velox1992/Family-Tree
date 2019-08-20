f152020027a0e367d45a7c0a69c8ce7213625012
Georg Braun <georg.braun@inform-software.com>
2019-08-20 10:09:52 +0200

    react digraph wieder entfernt

--- family-tree/src/App.js
+++ family-tree/src/App.js
@@ -1,18 +1,5 @@
 import React from "react";
 import "./App.css";
-import "./FamilyTreeView.css";
-import {
-  GraphView, // required
-  Edge, // optional
-  type IEdge, // optional
-  Node, // optional
-  type INode, // optional
-  type LayoutEngineType, // required to change the layoutEngineType, otherwise optional
-  BwdlTransformer, // optional, Example JSON transformer
-  GraphUtils // optional, useful utility functions
-} from 'react-digraph';
-
-var familyRelations = require("./familyRelations.json");
 
 class JsonEditor extends React.Component {
   render() {
@@ -20,133 +7,13 @@ class JsonEditor extends React.Component {
   }
 }
 
-const GraphConfig =  {
-  NodeTypes: {
-    empty: { // required to show empty nodes
-      typeText: "None",
-      shapeId: "#empty", // relates to the type property of a node
-      shape: (
-        <symbol viewBox="0 0 100 100" id="empty" key="0">
-          <circle cx="50" cy="50" r="45"></circle>
-        </symbol>
-      )
-    },
-    custom: { // required to show empty nodes
-      typeText: "Custom",
-      shapeId: "#custom", // relates to the type property of a node
-      shape: (
-        <symbol viewBox="0 0 50 25" id="custom" key="0">
-          <ellipse cx="50" cy="25" rx="50" ry="25"></ellipse>
-        </symbol>
-      )
-    }
-  },
-  NodeSubtypes: {},
-  EdgeTypes: {
-    emptyEdge: {  // required to show empty edges
-      shapeId: "#emptyEdge",
-      shape: (
-        <symbol viewBox="0 0 50 50" id="emptyEdge" key="0">
-          <circle cx="25" cy="25" r="8" fill="currentColor"> </circle>
-        </symbol>
-      )
-    }
-  }
-}
- 
-const NODE_KEY = "id"       // Allows D3 to correctly update DOM
-
-
 class FamilyGraph extends React.Component {
   constructor(props) {
     super(props);
- 
-    var sample = this.getGraphData();
-
-    this.state = {
-      graph: sample,
-      selected: {}
-    }
   }
 
-  getGraphData(){
-    return({
-      "nodes": [
-        {
-          "id": 1,
-          "title": "Node A",
-          "x": 258.3976135253906,
-          "y": 331.9783248901367,
-          "type": "empty"
-        },
-        {
-          "id": 2,
-          "title": "Node B",
-          "x": 593.9393920898438,
-          "y": 260.6060791015625,
-          "type": "empty"
-        },
-        {
-          "id": 3,
-          "title": "Node C",
-          "x": 237.5757598876953,
-          "y": 61.81818389892578,
-          "type": "custom"
-        },
-        {
-          "id": 4,
-          "title": "Node C",
-          "x": 600.5757598876953,
-          "y": 600.81818389892578,
-          "type": "custom"
-        }
-      ],
-      "edges": [
-        {
-          "source": 1,
-          "target": 2,
-          "type": "emptyEdge"
-        },
-        {
-          "source": 2,
-          "target": 4,
-          "type": "emptyEdge"
-        }
-      ]
-    });
-  }
- 
-  /* Define custom graph editing methods here */
- 
   render() {
-    const nodes = this.state.graph.nodes;
-    const edges = this.state.graph.edges;
-    const selected = this.state.selected;
- 
-    const NodeTypes = GraphConfig.NodeTypes;
-    const NodeSubtypes = GraphConfig.NodeSubtypes;
-    const EdgeTypes = GraphConfig.EdgeTypes;
- 
-    return (
-      <div id='graph' >
-        <GraphView  ref='GraphView'
-                    nodeKey={NODE_KEY}
-                    nodes={nodes}
-                    edges={edges}
-                    selected={selected}
-                    nodeTypes={NodeTypes}
-                    nodeSubtypes={NodeSubtypes}
-                    edgeTypes={EdgeTypes}
-                    onSelectNode={this.onSelectNode}
-                    onCreateNode={this.onCreateNode}
-                    onUpdateNode={this.onUpdateNode}
-                    onDeleteNode={this.onDeleteNode}
-                    onSelectEdge={this.onSelectEdge}
-                    onCreateEdge={this.onCreateEdge}
-                    onSwapEdge={this.onSwapEdge}
-                    onDeleteEdge={this.onDeleteEdge}/>
-      </div>
-    );
+    return <h2>Graph</h2>;
   }
 }
 
@@ -154,7 +21,7 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      familyRelationStructure: JSON.stringify(familyRelations),
+      //familyRelationStructure: JSON.stringify(familyRelations),
     };
 
     this.JsonValueChanged = this.JsonValueChanged.bind(this);
@@ -165,14 +32,15 @@ class App extends React.Component {
   }
 
   render() {
-
-    let hFamilyRelationStructure
-
     return (
       <div className="App">
-        <h1>Familienstammbaum</h1>
         <FamilyGraph />
-        {/*<JsonEditor value={this.state.familyRelationStructure} onChange={this.JsonValueChanged} />*/}
+        {
+          <JsonEditor
+            value={this.state.familyRelationStructure}
+            onChange={this.JsonValueChanged}
+          />
+        }
       </div>
     );
   }
