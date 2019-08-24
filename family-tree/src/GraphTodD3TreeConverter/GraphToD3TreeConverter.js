097586db518e946f2a884fc03d5f1fbcbc53835e
Georg Braun <Georg.Braun92@gmail.com>
2019-08-24 12:18:16 +0200

    Converter verhindert bei einem gleichen Graph einen erneuten Import

--- family-tree/src/GraphTodD3TreeConverter/GraphToD3TreeConverter.js
+++ family-tree/src/GraphTodD3TreeConverter/GraphToD3TreeConverter.js
@@ -1,10 +1,22 @@
 const FamilyGraphImporter = require("./FamilyGraphImporter");
 const FamilyToD3TreeConverter = require("./FamilyToD3TreeConverter");
 
+// Remember the last Graph/Family to avoid redundant import
+var previousGraph;
+var previousFamily;
 
 function convert(graphData, rootId){
-    var hImporter = new FamilyGraphImporter();
-    var hFamily = hImporter.import(graphData);
+
+    var hFamily = previousFamily;
+    
+    if (graphData !== previousGraph){
+        console.log("New import!");
+        var hImporter = new FamilyGraphImporter();
+        hFamily = hImporter.import(graphData);      
+
+        previousGraph = graphData;
+        previousFamily = hFamily;
+    }
 
     return FamilyToD3TreeConverter(hFamily, rootId);
 
