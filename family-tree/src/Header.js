f20093e4674325846e623de0eed1ac2ca9aee5b8
Georg Braun <Georg.Braun92@gmail.com>
2019-09-02 10:12:35 +0200

    Hilfe Dialog ergänzt

--- family-tree/src/Header.js
+++ family-tree/src/Header.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTree, faCode } from "@fortawesome/free-solid-svg-icons";
+import {
+  faTree,
+  faCode,
+  faQuestionCircle
+} from "@fortawesome/free-solid-svg-icons";
 
 class Header extends React.Component {
+  showHelpClick(event) {
+    console.log("HELP");
+  }
+
   render() {
     return (
       <Navbar bg="dark">
@@ -17,7 +26,9 @@ class Header extends React.Component {
         <Navbar.Collapse className="justify-content-end">
           <Navbar.Text>
             <span className="white-icon">
+              <HelpDialog></HelpDialog>
               <Button
+                className="header-button"
                 onClick={this.props.onShowDataClick}
               >
                 <FontAwesomeIcon size="2x" icon={faCode} />
@@ -28,6 +39,57 @@ class Header extends React.Component {
       </Navbar>
     );
   }
+}
+
+function HelpDialog() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <Button className="header-button" variant="primary" onClick={handleShow}>
+        <FontAwesomeIcon size="2x" icon={faQuestionCircle} />
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Hilfe Familienstammbaum</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p>
+            Der Familienstammbaum zeigt den Stammbaum für ein ausgewähltes
+            Familienmitglied an. Durch einen Linksklick auf ein Familienmitglied
+            wird der Stammbaum entsprechend angepasst.
+          </p>
+          <p>
+            Sollte ein Familienmitglied noch weitere Eltern-Informationen
+            enthalten, die in der aktuellen Darstellung nicht dargestellt
+            werden, so wird dieses Familienmitglied visuell hervorgehoben.
+          </p>
+          <span className="family-node-link-example">Magda Muster</span>
+          <p></p>
+          <span>
+            <FontAwesomeIcon
+              className="header-button"
+              size="2x"
+              icon={faCode}
+            />
+          </span>
+          <p>
+            Öffnet den Editor, welcher die für den Familienstammbaum genutzten
+            Daten anzeigt.
+          </p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="primary" onClick={handleClose}>
+            Schließen
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
 }
 
 export default Header;
